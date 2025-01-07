552df8afd215ae752e4ab287d14584769203337a
Virgers <rawking1621@gmail.com>
2025-01-07 21:54:41 +0800

    update earlystopping

--- utils/tools.py
+++ utils/tools.py
@@ -6,7 +6,6 @@ import torch
 
 class EarlyStopping:
     def __init__(self, patience=7, verbose=False, delta=0):
-        # self.attr_name = attr_name
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
@@ -16,12 +15,11 @@ class EarlyStopping:
         self.delta = delta
 
     def __call__(self, val_loss, model, path, attr_name, stage_name):
-        score = -val_loss
+        score = -val_loss  # Lower val_loss is better, so we negate it
         if self.best_score is None:
             self.best_score = score
             self.save_checkpoint(val_loss, model, path, attr_name, stage_name)
-            # self.save_attns(attns, path)
-        elif score < self.best_score + self.delta:
+        elif score > self.best_score + self.delta:  # Changed < to >
             self.counter += 1
             print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
@@ -30,11 +28,10 @@ class EarlyStopping:
             self.best_score = score
             self.save_checkpoint(val_loss, model, path, attr_name, stage_name)
             self.counter = 0
-  
+
     def save_checkpoint(self, val_loss, model, path, attr_name, stage_name):
         if self.verbose:
             print(f'Validation loss decreased ({self.val_loss_min:.4f} --> {val_loss:.4f}). Saving model ...')
         checkpoint_path = f"{path}/{stage_name}_{attr_name}_checkpoint.pth"
         torch.save(model.state_dict(), checkpoint_path)
         self.val_loss_min = val_loss
-
