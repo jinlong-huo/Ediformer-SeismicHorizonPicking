6131f3d2d639e54240af80dc934506357c2b2f36
Virgers <rawking1621@gmail.com>
2025-02-18 18:59:04 +0100

    update diformer patch

--- models/diformer_patch.py
+++ models/diformer_patch.py
@@ -30,29 +30,29 @@ def weight_init(m):
             torch.nn.init.zeros_(m.bias)  #
 
 
-class CoFusion(nn.Module):
+# class CoFusion(nn.Module):
 
-    def __init__(self, in_ch, out_ch):
-        super(CoFusion, self).__init__()
-        self.conv1 = nn.Conv2d(in_ch, 64, kernel_size=1,
-                               stride=1, padding=1)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=1,
-                               stride=1, padding=1)
-        self.conv3 = nn.Conv2d(64, out_ch, kernel_size=1,
-                               stride=1, padding=1)
-        self.relu = nn.ReLU()   
+#     def __init__(self, in_ch, out_ch):
+#         super(CoFusion, self).__init__()
+#         self.conv1 = nn.Conv2d(in_ch, 64, kernel_size=1,
+#                                stride=1, padding=1)
+#         self.conv2 = nn.Conv2d(64, 64, kernel_size=1,
+#                                stride=1, padding=1)
+#         self.conv3 = nn.Conv2d(64, out_ch, kernel_size=1,
+#                                stride=1, padding=1)
+#         self.relu = nn.ReLU()   
 
-        self.norm_layer1 = nn.GroupNorm(4, 64)
-        self.norm_layer2 = nn.GroupNorm(4, 64)
+#         self.norm_layer1 = nn.GroupNorm(4, 64)
+#         self.norm_layer2 = nn.GroupNorm(4, 64)
 
-    def forward(self, x):
-        # fusecat = torch.cat(x, dim=1)
-        attn = self.relu(self.norm_layer1(self.conv1(x)))
-        attn = self.relu(self.norm_layer2(self.conv2(attn)))
-        attn = F.softmax(self.conv3(attn), dim=1)
+#     def forward(self, x):
+#         # fusecat = torch.cat(x, dim=1)
+#         attn = self.relu(self.norm_layer1(self.conv1(x)))
+#         attn = self.relu(self.norm_layer2(self.conv2(attn)))
+#         attn = F.softmax(self.conv3(attn), dim=1)
 
-        # return ((fusecat * attn).sum(1)).unsqueeze(1)
-        return ((x * attn).sum(1)).unsqueeze(1)
+#         # return ((fusecat * attn).sum(1)).unsqueeze(1)
+#         return ((x * attn).sum(1)).unsqueeze(1)
 
 
 class _DenseLayer(nn.Sequential):
