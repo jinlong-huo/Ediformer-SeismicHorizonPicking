998f6f40a08a19685905fbfee97b2af9218b5e7e
Virgers <rawking1621@gmail.com>
2025-05-25 20:47:28 +0800

    Add .gitignore entries and untrack ignored files

--- .history/utils/preprocess_amp_20241226104554.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-data = np.load('/home/dell/disk1/Jinlong/Horizontal-data/F3_amp.npy')
-
-for i in range(1, data.shape[0]):
-    for j in range(data.shape[1]):  # Iterate over columns
-        if np.isinf(data[i, j]):
-            data[i, j] = data[i, j-1]   
-np.save('/home/dell/disk1/Jinlong/Horizontal-data/F3_amp_modified.npy')
