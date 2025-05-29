8c92a1874ae850b418b1e9166eb3c78fcebd3799
Virgers <rawking1621@gmail.com>
2025-05-29 13:03:20 +0800

    modify base models

--- Ediformer/models/diformer_patch_attn.py
+++ Ediformer/models/diformer_patch_attn.py
@@ -136,49 +136,6 @@ class DoubleConvBlock(nn.Module):
 
         return x
 
-
-class PatchEmbed(nn.Module):
-    """ 2D Image to Patch Embedding
-    """
-    def __init__(self, img_size=224, kernel_size=16,  stride=16, padding=0, in_chans=3, embed_dim=768):
-        super().__init__()
-        kernel_size = to_2tuple(kernel_size)
-        stride = to_2tuple(stride)
-        padding = to_2tuple(padding)
-        
-        
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=kernel_size, stride=stride, padding=padding )
-        self.norm = nn.BatchNorm2d(embed_dim)
-
-    def forward(self, x):
-        # B, C, H, W = x.shape
-        x = self.proj(x)
-        x = self.norm(x)
-        x = x.permute(0,2,3,1)
-        return x
-
-class FirstPatchEmbed(nn.Module):
-    """ 2D Image to Patch Embedding
-    """
-    def __init__(self, kernel_size=3,  stride=2, padding=1, in_chans=3, embed_dim=768):
-        super().__init__()
-        
-        self.proj1 = nn.Conv2d(in_chans, embed_dim//2, kernel_size=kernel_size, stride=stride, padding=padding )
-        self.norm1 = nn.BatchNorm2d(embed_dim // 2)
-        self.gelu1 = nn.GELU()
-        self.proj2 = nn.Conv2d(embed_dim//2, embed_dim, kernel_size=kernel_size, stride=stride, padding=padding )
-        self.norm2 = nn.BatchNorm2d(embed_dim)
-        
-    def forward(self, x):
-        # B, C, H, W = x.shape
-        x = self.proj1(x)
-        x = self.norm1(x)
-        x = self.gelu1(x)
-        x = self.proj2(x)
-        x = self.norm2(x)    
-        x = x.permute(0,2,3,1)
-        return x
-    
 class HighMixer(nn.Module):
     def __init__(self, dim, kernel_size=3, stride=1, padding=1,
         **kwargs, ):
